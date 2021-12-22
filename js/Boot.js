9839de03caed7f3fe0ee4e417ab8b51f2d12445f
Failproo <90498067+Failproo@users.noreply.github.com>
2021-12-22 20:24:12 +0530

    Add files via upload

--- js/Boot.js
+++ js/Boot.js
@@ -1,1 +1,23 @@
-
+class Boot extends Phaser.Scene {
+  constructor() {
+    super('Boot');
+  }
+  
+  preload () {
+    this.load.image('logo', 'assets/logo.png')
+  }
+  
+  create() {
+    const Logo = this.add.image(400,150,'logo')
+    
+    this.tweens.add({
+            targets: logo,
+            y: 550,
+            duration: 2000,
+            ease: "Power2",
+            yoyo: true,
+            loop: -1
+        });
+        
+  }
+}
---
+++ js/index.js
@@ -0,0 +1,9 @@
+const config = {
+    type: Phaser.AUTO,
+    parent: 'phaser-example',
+    width: 800,
+    height: 600,
+    scene:['Boot']
+};
+
+const game = new Phaser.Game(config);
